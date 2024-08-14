1200d43636cd301f136f9befb75429722be93b31
kykydz <zkyhersa@gmail.com>
2024-08-14 14:36:54 +0700

    init

--- assignments/main.go
+++ assignments/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+)
 
 func main() {
 	fmt.Println("hello world")
@@ -9,8 +13,8 @@ func main() {
 	// arraySign([]int{-2, 1})                   // -1
 	// arraySign([]int{-1, -2, -3, -4, 3, 2, 1}) // 1
 
-	// isAnagram("anak", "kana") // true
-	// isAnagram("anak", "mana") // false
+	// isAnagram("anak", "kana")       // true
+	// isAnagram("anak", "mana")       // false
 	// isAnagram("anagram", "managra") // true
 
 	// findTheDifference("abcd", "abcde") // 'e'
@@ -21,34 +25,92 @@ func main() {
 	// canMakeArithmeticProgression([]int{5, 1, 9})    // true; 9, 5, 1 adalah baris aritmatik -4
 	// canMakeArithmeticProgression([]int{1, 2, 4, 8}) // false; 1, 2, 4, 8 bukan baris aritmatik, melainkan geometrik x2
 
-	// tesDeck()
+	tesDeck()
 }
 
 // https://leetcode.com/problems/sign-of-the-product-of-an-array
 func arraySign(nums []int) int {
-	// write code here
+	productSign := 1
 
-	return 1 // if positive
-	// return -1 // if negative
+	for _, num := range nums {
+		if num == 0 {
+			return 0
+		} else if num < 0 {
+			productSign *= -1
+		}
+	}
+
+	fmt.Println(productSign)
+	return productSign
 }
 
 // https://leetcode.com/problems/valid-anagram
 func isAnagram(s string, t string) bool {
-	// write code here
-	return false
+	if len(s) != len(t) {
+		fmt.Println(false)
+		return false
+	}
+
+	count := make(map[rune]int)
+
+	for _, char := range s {
+		count[char]++
+	}
+
+	for _, char := range t {
+		count[char]--
+		if count[char] < 0 {
+			fmt.Println(false)
+			return false
+		}
+	}
+
+	fmt.Println(true)
+	return true
 }
 
 // https://leetcode.com/problems/find-the-difference
 func findTheDifference(s string, t string) byte {
-	// write code here
-	b := byte('a')
-	return b
+	// Create a map to count the occurrences of each character
+	count := make(map[rune]int)
+
+	// Count characters in string `s`
+	for _, char := range s {
+		count[char]++
+	}
+
+	// Subtract counts using characters in string `t`
+	for _, char := range t {
+		count[char]--
+		// If count becomes negative, this character is the extra one in `t`
+		if count[char] < 0 {
+			fmt.Printf("%c\n", byte(char))
+			return byte(char)
+		}
+	}
+
+	// This point should not be reached, given valid input constraints
+	return 0
 }
 
 // https://leetcode.com/problems/can-make-arithmetic-progression-from-sequence
 func canMakeArithmeticProgression(arr []int) bool {
-	// write code here
-	return false
+	// Sort the array
+	sort.Ints(arr)
+
+	// Calculate the difference between the first two elements
+	diff := arr[1] - arr[0]
+
+	// Check if the difference is the same for all consecutive elements
+	for i := 2; i < len(arr); i++ {
+		if arr[i]-arr[i-1] != diff {
+			fmt.Println(false)
+			return false
+		}
+	}
+
+	fmt.Println(true, diff)
+	return true
 }
 
 // Deck represent "standard" deck consist of 52 cards
@@ -66,19 +128,29 @@ type Card struct {
 // [A Spade, 2 Spade,  ..., A Heart, 2 Heart, ..., J Diamond, Q Diamond, K Diamond ]
 // assume Ace-Spade on top of deck.
 func (d *Deck) New() {
-	// write code here
+	d.cards = []Card{}
+	for symbol := 0; symbol <= 3; symbol++ {
+		for number := 1; number <= 13; number++ {
+			d.cards = append(d.cards, Card{symbol: symbol, number: number})
+		}
+	}
 }
 
 // PeekTop return n cards from the top
 func (d Deck) PeekTop(n int) []Card {
-	// write code here
-	return nil
+	// if the left cards on the deck is less than n peek
+	if n > len(d.cards) {
+		n = len(d.cards)
+	}
+	return d.cards[:n]
 }
 
 // PeekTop return n cards from the bottom
 func (d Deck) PeekBottom(n int) []Card {
-	// write code here
-	return nil
+	if n > len(d.cards) {
+		n = len(d.cards)
+	}
+	return d.cards[len(d.cards)-n:]
 }
 
 // PeekCardAtIndex return a card at specified index
@@ -88,13 +160,18 @@ func (d Deck) PeekCardAtIndex(idx int) Card {
 
 // Shuffle randomly shuffle the deck
 func (d *Deck) Shuffle() {
-	// write code here
+	rand.Shuffle(len(d.cards), func(i, j int) {
+		d.cards[i], d.cards[j] = d.cards[j], d.cards[i]
+	})
 }
 
 // Cut perform single "Cut" technique. Move n top cards to bottom
 // e.g. Deck: [1, 2, 3, 4, 5]. Cut(3) resulting Deck: [4, 5, 1, 2, 3]
 func (d *Deck) Cut(n int) {
-	// write code here
+	if n <= 0 || n >= len(d.cards) {
+		return
+	}
+	d.cards = append(d.cards[n:], d.cards[:n]...)
 }
 
 func (c Card) ToString() string {
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/afteroffice/go-basics
 
-go 1.22.4
+go 1.22
+
+require golang.org/x/exp v0.0.0-20240808152545-0cdaa3abc0fa // indirect
